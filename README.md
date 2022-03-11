1b591511d567b6050348035b7c8ba166ab8d79cc
Shadow <95940556+Sallat-M@users.noreply.github.com>
2022-03-11 18:11:09 +0200

    first-commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1 align="center">Bot Status Checker</h1>
+
+<hr>
+
+<h3 align='center'>
+    This bot will check bots listed in a channel and add mark if any bot is offline
+</h3>
+
+
+## Deploy the Bot
+
+Press the below button to deploy to Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+
+
+## Contact me
+
+[![Telegram Channel](https://img.shields.io/static/v1?label=Join&message=Telegram%20Channel&color=blueviolet&style=for-the-badge&logo=telegram&logoColor=violet)](https://telegram.me/shadow_bots)
+
+
+You can PM me on [@ooom09](https://telegram.me/ooom09)
---
+++ main.py
@@ -0,0 +1,80 @@
+from asyncio import sleep
+import os
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+session = os.environ.get("String_Session")
+chat = os.environ.get("Channel_ID")
+offline_mark = os.environ.get("Offline_Mark")
+send_time = int(os.environ.get("Send_Time"))
+time_out = int(os.environ.get("Time_Out"))
+sleep_time = int(os.environ.get("Sleep_Time"))
+
+
+client = Client(session, api_id, api_hash)
+bot = {"username": None, "status": False}
+
+
+async def main():
+    while True:
+        async for msg in client.iter_history(chat, reverse=True):
+            if msg.text:
+                try:
+                    await check_msg(msg)
+                except Exception as e:
+                    print(f"Error: {e}")
+        await sleep(sleep_time)
+
+
+async def check_msg(msg: Message):
+    text = ""
+    edit = False
+    for line in msg.text.markdown.split('\n'):
+        for word in line.split():
+            if word.startswith('@') and word.lower().endswith('bot'):
+                off = line.find(offline_mark)
+                await sleep(send_time)
+                try:
+                    work = await check_bot(word)
+                except Exception as e:
+                    print(f"Error: {e}")
+                    continue
+                if work:
+                    if off > -1:
+                        text += f"\n{line[:off - 1]}"
+                        edit = True
+                    else:
+                        text += f"\n{line}"
+                else:
+                    if off > -1:
+                        text += f"\n{line}"
+                    else:
+                        text += f"\n{line} {offline_mark}"
+                        edit = True
+                break
+        else:
+            text += f"\n{line}"
+    if edit:
+        await msg.edit_text(text, reply_markup=msg.reply_markup)
+
+
+async def check_bot(username):
+    bot["username"] = username[1:]
+    bot["status"] = False
+    await client.send_message(username, "/start")
+    await sleep(time_out)
+    return bot["status"]
+
+
+@client.on_message(filters.bot)
+async def response(_, msg):
+    if msg.chat.username == bot["username"]:
+        bot["status"] = True
+
+
+client.start()
+client.loop.run_until_complete(main())
